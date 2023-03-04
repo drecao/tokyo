fc8d24d8f1a4d8a0321ee0b75796e2936cdf4e30
macbook-pro <3101158440@qq.com>
2023-03-04 18:03:29 +0800

    test-on-my-vscode

--- test.c
+++ test.c
@@ -4,6 +4,8 @@ int main()
 	printf("test\n");
 	printf("test again\n");
 	printf("test push\n");
+	printf("test on my vscode\n");
+	/*test on may vscscode*/
 	/*test test on mac*/
 	/*testggdhgjhghbj*/
 	return 0;
